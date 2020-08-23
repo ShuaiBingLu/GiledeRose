821e908b1f36cb748815921b90955787c3b80dcf
Lu Bingshuai <bingshuai.lu@thoughtworks.com>
2020-08-23 21:40:21 +0800

    refactor

--- src/main/java/cn/xpbootcamp/gildedrose/Goods.java
+++ src/main/java/cn/xpbootcamp/gildedrose/Goods.java
@@ -1,6 +1,10 @@
 package cn.xpbootcamp.gildedrose;
 
 public class Goods {
+
+    private static final String BACKSTAGE_PASS_GOODS = "BackstagePass";
+
+    private static final String REGULAR_GOODS = "Regular";
     private int sellIn;
 
     private String goodsType;
@@ -9,16 +13,8 @@ public class Goods {
         return sellIn;
     }
 
-    public void setSellIn(int sellIn) {
-        this.sellIn = sellIn;
-    }
-
     public double getQuality() {
         return quality;
-    }
-
-    public void setQuality(int quality) {
-        this.quality = quality;
     }
 
     private double quality;
@@ -30,28 +26,40 @@ public class Goods {
         this.goodsType = goodsType;
     }
 
-    public void update() {
-        if (goodsType.equals("Regular")) {
+    private void updateRegularGoods() {
+        decreaseQuality();
+        sellIn = sellIn - 1;
+        if (sellIn < 0) {
             decreaseQuality();
-            sellIn = sellIn - 1;
-            if (sellIn < 0) {
-                decreaseQuality();
-            }
-        } else {
-            if (sellIn > 10) {
-                quality = quality + 1;
-            } else if (sellIn > 5) {
-                quality = quality + 2;
-            } else if (sellIn > 0) {
-                quality = quality + 3;
-            } else {
-                quality = 0;
-            }
-            if (quality >= 50) {
-                quality = 50;
-            }
-            sellIn = sellIn - 1;
         }
+    }
+
+    private void updateStagePassGoods() {
+        if (sellIn > 10) {
+            increaseQuality(1);
+        } else if (sellIn > 5) {
+            increaseQuality(2);
+        } else if (sellIn > 0) {
+            increaseQuality(3);
+        } else {
+            quality = 0;
+        }
+        if (quality >= 50) {
+            quality = 50;
+        }
+        sellIn = sellIn - 1;
+    }
+
+    public void update() {
+        if (REGULAR_GOODS.equals(goodsType)) {
+            updateRegularGoods();
+        } else if (BACKSTAGE_PASS_GOODS.equals(goodsType)) {
+            updateStagePassGoods();
+        }
+    }
+
+    private void increaseQuality(double num) {
+        quality = quality + num;
     }
 
     private void decreaseQuality() {
